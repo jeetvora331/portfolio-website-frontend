7cdeb3feafdc1277ca9e052fee126d457855c0b4
Jeet Vora <jeetvora331@gmail.com>
2026-04-30 02:59:59 +0530

    skills updated

--- src/data/skills.js
+++ src/data/skills.js
@@ -1,18 +1,15 @@
-import {
-	default as css,
-	default as csss,
-} from "../../public/images/toolbox/css.png";
+import { default as css } from "../../public/images/toolbox/css.png";
 import figma from "../../public/images/toolbox/figma.png";
 import github from "../../public/images/toolbox/github.png";
 import html from "../../public/images/toolbox/html.png";
-import java from "../../public/images/toolbox/java.png";
 import javascript from "../../public/images/toolbox/javascript.png";
 import nextjs from "../../public/images/toolbox/nextjs.png";
 import python from "../../public/images/toolbox/python.png";
 import react from "../../public/images/toolbox/react.png";
-import strapi from "../../public/images/toolbox/strapi.png";
 import tailwind from "../../public/images/toolbox/tailwind.png";
-import xd from "../../public/images/toolbox/xd.png";
+import typescript from "../../public/images/toolbox/typescript.png";
+import supabase from "../../public/images/toolbox/supabase.png";
+import claude from "../../public/images/toolbox/claude.png";
 
 const skills = [
 	{
@@ -20,8 +17,8 @@ const skills = [
 		src: python,
 	},
 	{
-		title: "Java",
-		src: java,
+		title: "TypeScript",
+		src: typescript,
 	},
 	{
 		title: "JavaScript",
@@ -52,19 +49,17 @@ const skills = [
 		title: "GitHub",
 		src: github,
 	},
-
-	{
-		title: "XD",
-		src: xd,
-	},
-
 	{
 		title: "Figma",
 		src: figma,
 	},
 	{
-		title: "Strapi",
-		src: strapi,
+		title: "Supabase",
+		src: supabase,
+	},
+	{
+		title: "Claude",
+		src: claude,
 	},
 ];
 export default skills;
